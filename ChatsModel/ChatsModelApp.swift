efc81bef23b2f79f9381669eedb596064077f1d4
Razan <razanalzannan@gmail.com>
2023-05-22 09:44:10 +0300

    Initial Commit

---
+++ ChatsModel/ChatsModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatsModelApp.swift
+//  ChatsModel
+//
+//  Created by Razan Mohammed Alzannan on 02/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct ChatsModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
